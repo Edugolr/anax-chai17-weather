60d038b2dad347e38f1e8e76e3af2ff4fc0fcf94
Edugolr <christofer.wikman@gmail.com>
2018-11-28 18:36:06 +0100

    had to bring alot more config files than intended updated and added tough

--- test/config/page.php
+++ test/config/page.php
@@ -47,50 +47,5 @@ return [
                 "siteLogoTextIconAlt" => "Löv-bild",
             ],
         ],
-        // [
-        //     "region" => "header-col-2",
-        //     "template" => "anax/v2/navbar/navbar_submenus",
-        //     "data" => [
-        //         "navbarConfig" => require __DIR__ . "/navbar/header.php",
-        //     ],
-        // ],
-        // [
-        //     "region" => "header-col-3",
-        //     "template" => "anax/v2/navbar/responsive_submenus",
-        //     "data" => [
-        //         "navbarConfig" => require __DIR__ . "/navbar/responsive.php",
-        //     ],
-        // ],
-        [
-            "region" => "footer",
-            "template" => "anax/v2/columns/multiple_columns",
-            "data" => [
-                "class"  => "footer-column",
-                "columns" => [
-                    [
-                        "template" => "anax/v2/block/default",
-                        "contentRoute" => "block/footer-col-1",
-                    ],
-                    [
-                        "template" => "anax/v2/block/default",
-                        "contentRoute" => "block/footer-col-2",
-                    ],
-                    [
-                        "template" => "anax/v2/block/default",
-                        "contentRoute" => "block/footer-col-3",
-                    ]
-                ]
-            ],
-            "sort" => 1
-        ],
-        [
-            "region" => "footer",
-            "template" => "anax/v2/block/default",
-            "data" => [
-                "class"  => "site-footer",
-                "contentRoute" => "block/footer",
-            ],
-            "sort" => 2
-        ],
     ],
 ];
