04bf242e7461b36d8cd181549b168a9f89c6885b
Edugolr <christofer.wikman@gmail.com>
2018-11-29 15:14:31 +0100

    improving security

--- view/weather/index.php
+++ view/weather/index.php
@@ -2,17 +2,17 @@
 namespace Anax\View;
 
 ?>
-Välj prognos eller gamla rapporter 
+Välj prognos eller gamla rapporter
 <div class="">
     <form action="">
-      <input type="radio" name="action" onclick="document.getElementById('weather').action='<?= url("weather/weather") ?>';"> Vädret nu och kommande<br>
-      <input type="radio" name="action" onclick="document.getElementById('weather').action='<?= url("weather/weatherOld") ?>';"> Vädret gammalt (30 dagar)<br>
+      <input type="radio" name="action" onclick="document.getElementById('weather').action='<?=htmlentities(url("weather/weather")) ?>';"> Vädret nu och kommande<br>
+      <input type="radio" name="action" onclick="document.getElementById('weather').action='<?= htmlentities(url("weather/weatherOld")) ?>';"> Vädret gammalt (30 dagar)<br>
     </form>
 </div> <br><br>
 Ange ip eller ort för väderleksrapport
 <div class="">
-    <form id="weather" class="" action='<?= url("weather/weather") ?>' method="get">
-        Ip:<input type="text" name="ip" value="<?=$ip ?>">
+    <form id="weather" class="" action='<?= htmlentities(url("weather/weather")) ?>' method="get">
+        Ip:<input type="text" name="ip" value="<?=htmlentities($ip) ?>">
         <input class="btn" type="submit" name="" value="Submit">
     </form>
 </div>
