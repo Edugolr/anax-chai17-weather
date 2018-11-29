fb55ce938b3d78fc1fb9634451ca7c8dd66f87b8
Edugolr <christofer.wikman@gmail.com>
2018-11-29 15:22:32 +0100

    improving security

--- view/weather/weather.php
+++ view/weather/weather.php
@@ -3,9 +3,9 @@ namespace Anax\View;
 
 if (isset($location)) {
     ?>
-    Stad:   <?= $location["city"] ?> <br>
-    Lat:    <?= $location["latitude"] ?> <br>
-    Long:   <?= $location["longitude"] ?> <br>
+    Stad:   <?= htmlentities($location["city"]) ?> <br>
+    Lat:    <?= htmlentities($location["latitude"]) ?> <br>
+    Long:   <?= htmlentities($location["longitude"]) ?> <br>
     <?php
 }
 
@@ -15,22 +15,22 @@ if (isset($weather["currently"])) {
     <div class="container days">
         <div class="container card  current">
             <i class="<?=$weather["currently"]["icon"] ?>"></i>
-            Vädret just nu: <?=  strftime("%c", $weather["currently"]["time"])?> <br>
-            Väderlek  : <?= $weather["currently"]["summary"] ?><br>
-            Temperatur: <?= $weather["currently"]["temperature"] ?> &#8451;<br>
-            windSpeed : <?= $weather["currently"]["windSpeed"] ?> m/s
+            Vädret just nu: <?=  htmlentities(strftime("%c", $weather["currently"]["time"]))?> <br>
+            Väderlek  : <?= htmlentities($weather["currently"]["summary"]) ?><br>
+            Temperatur: <?= htmlentities($weather["currently"]["temperature"]) ?> &#8451;<br>
+            windSpeed : <?= htmlentities($weather["currently"]["windSpeed"]) ?> m/s
         </div> <br><br>
         <?php
 
         foreach ($weather["daily"]["data"] as $key => $value) {
             ?>
             <div class="container card day">
-                <i class="<?=$value["icon"]?>"></i>
+                <i class="<?=htmlentities($value["icon"])?>"></i>
                 Dag: <?= ucfirst(strftime("%A", $value["time"])); ?> | <?=  strftime("%x", $value["time"])?> <br>
-                Väderlek  : <?= $value["summary"] ?><br>
-                Temperatur (högsta): <span class="tempHigh"><?= $value["temperatureHigh"] ?></span>  &#8451; <br>
-                Temperatur (lägsta): <span class="tempLow"><?= $value["temperatureLow"] ?></span> &#8451;<br>
-                VindHastighet : <?= $value["windSpeed"] ?> m/s<br>
+                Väderlek  : <?= htmlentities($value["summary"]) ?><br>
+                Temperatur (högsta): <span class="tempHigh"><?= htmlentities($value["temperatureHigh"]) ?></span>  &#8451; <br>
+                Temperatur (lägsta): <span class="tempLow"><?= htmlentities($value["temperatureLow"]) ?></span> &#8451;<br>
+                VindHastighet : <?= htmlentities($value["windSpeed"]) ?> m/s<br>
             </div> <br><br>
                 <?php
         }
--- view/weather/weatherOld.php
+++ view/weather/weatherOld.php
@@ -3,26 +3,25 @@ namespace Anax\View;
 
 if (isset($location)) {
     ?>
-    Stad:   <?= $location["city"] ?> <br>
-    Lat:    <?= $location["latitude"] ?> <br>
-    Long:   <?= $location["longitude"] ?> <br>
+    Stad:   <?= htmlentities($location["city"]) ?> <br>
+    Lat:    <?= htmlentities($location["latitude"]) ?> <br>
+    Long:   <?= htmlentities($location["longitude"]) ?> <br>
     <?php
 }
 setlocale(LC_ALL, 'swedish.UTF-8');
 if (isset($weather[0]["daily"])) {
-    var_dump($weather);
     ?>
     <div class="container days">
     <?php
     for ($i=0; $i < count($weather); $i++) {
         ?>
         <div class="container card day">
-            <i class="<?=$weather[$i]["daily"]["data"][0]["icon"]?>"></i>
-            Dag: <?= ucfirst(strftime("%A", $weather[$i]["currently"]["time"])); ?> | <?=  strftime("%x", $weather[$i]["currently"]["time"])?> <br>
-            Väderlek  : <?= $weather[$i]["daily"]["data"][0]["summary"] ?><br>
-            Temperatur (högsta): <span class="tempHigh"><?=$weather[$i]["daily"]["data"][0]["temperatureHigh"] ?></span>  &#8451; <br>
-            Temperatur (lägsta): <span class="tempLow"><?= $weather[$i]["daily"]["data"][0]["temperatureLow"] ?></span> &#8451;<br>
-            VindHastighet : <?= $weather[$i]["currently"]["windSpeed"] ?> m/s<br>
+            <i class="<?=htmlentities($weather[$i]["daily"]["data"][0]["icon"])?>"></i>
+            Dag: <?= htmlentities(ucfirst(strftime("%A", $weather[$i]["currently"]["time"]))); ?> | <?=  htmlentities(strftime("%x", $weather[$i]["currently"]["time"]))?> <br>
+            Väderlek  : <?= htmlentities($weather[$i]["daily"]["data"][0]["summary"]) ?><br>
+            Temperatur (högsta): <span class="tempHigh"><?=htmlentities($weather[$i]["daily"]["data"][0]["temperatureHigh"]) ?></span>  &#8451; <br>
+            Temperatur (lägsta): <span class="tempLow"><?= htmlentities($weather[$i]["daily"]["data"][0]["temperatureLow"]) ?></span> &#8451;<br>
+            VindHastighet : <?= htmlentities($weather[$i]["currently"]["windSpeed"]) ?> m/s<br>
         </div> <br><br>
         <?php
     }
